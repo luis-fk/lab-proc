468c66edf8463b71cdbe352c813e1dde10fb6750
Davi Félix <davifelix@usp.br>
2025-07-25 22:18:27 -0300

    configurando memória virtual

--- memoria/mem.c
+++ memoria/mem.c
@@ -20,7 +20,7 @@ typedef struct {
 /**
  * TTB no endereço 0x4000 (fixo e alinhado em 16 kiB).
  */
-ttb_l1_t *ttb_l1 = (ttb_l1_t*)0x4000; /*Como esse endereço é decidido?*/
+ttb_l1_t *ttb_l1 = (ttb_l1_t*)0xF4000; /*Como esse endereço é decidido?*/
 
 /**
  * Desabilita o gerenciador de memória.
@@ -81,7 +81,7 @@ void map_section(uint32_t virt, uint32_t fis, uint32_t flags) {
  * @param virt Endereço virtual da seção.
  */
 void map_invalid(uint32_t virt) {
-   int indice = (virt & 0xfff00000) >> 20;              // índice do descritor, entre 0 e 4095 (12 bits do endereço virtual)
+   int indice = (virt & 0xfff00000) >> 20;     //0xf     // índice do descritor, entre 0 e 4095 (12 bits do endereço virtual)
    ttb_l1->descriptor[indice] = 0;
 }
 
@@ -96,13 +96,12 @@ void mmu_flat(void) {
 }
 
 int main(void) {
-   uint32_t *ptr = 0x0020fff0;
    mmu_flat();
    map_section(0x00100000, 0x00000000, 0x0000);    // mapeia o primeiro MiB da memória física no segundo MiB da memória virtual.
    map_invalid(0x00200000);                        // "desmapeia" o terceiro MiB da memória virtual
    tlb_invalida();
    mmu_start(ttb_l1);                              // liga o bicho
-   *ptr = 0x0de0c0c0;
+   mmu_stop();
    for(;;);
 }
 
